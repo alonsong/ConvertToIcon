bc3b53ae6918ddf0d633e999038b6f5fbdd02984
84789887 <84789887@qq.com>
2024-02-24 20:23:26 +0800

    项目初始化

---
+++ FormMainApp.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ImageToIconApp
+{
+    partial class FormMainApp
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainApp));
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
+            this.buttonChangeIcon = new System.Windows.Forms.Button();
+            this.textBoxImagePath = new System.Windows.Forms.TextBox();
+            this.openFileDialogImage = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(149, 66);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(117, 30);
+            this.btnSelectImage.TabIndex = 0;
+            this.btnSelectImage.Text = "PNG图片选择";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // comboBoxSize
+            // 
+            this.comboBoxSize.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Items.AddRange(new object[] {
+            "16*16",
+            "24*24",
+            "32*32",
+            "48*48",
+            "64*64",
+            "72*72",
+            "92*92",
+            "108*108"});
+            this.comboBoxSize.Location = new System.Drawing.Point(23, 25);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(95, 25);
+            this.comboBoxSize.TabIndex = 2;
+            this.comboBoxSize.Text = "16*16";
+            // 
+            // buttonChangeIcon
+            // 
+            this.buttonChangeIcon.Location = new System.Drawing.Point(328, 66);
+            this.buttonChangeIcon.Name = "buttonChangeIcon";
+            this.buttonChangeIcon.Size = new System.Drawing.Size(117, 30);
+            this.buttonChangeIcon.TabIndex = 4;
+            this.buttonChangeIcon.Text = "Icon转换";
+            this.buttonChangeIcon.UseVisualStyleBackColor = true;
+            this.buttonChangeIcon.Click += new System.EventHandler(this.buttonChangeIcon_Click);
+            // 
+            // textBoxImagePath
+            // 
+            this.textBoxImagePath.Enabled = false;
+            this.textBoxImagePath.Location = new System.Drawing.Point(124, 25);
+            this.textBoxImagePath.Name = "textBoxImagePath";
+            this.textBoxImagePath.Size = new System.Drawing.Size(501, 25);
+            this.textBoxImagePath.TabIndex = 5;
+            // 
+            // openFileDialogImage
+            // 
+            this.openFileDialogImage.RestoreDirectory = true;
+            this.openFileDialogImage.Title = "图片选择";
+            // 
+            // FormMainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 130);
+            this.Controls.Add(this.textBoxImagePath);
+            this.Controls.Add(this.buttonChangeIcon);
+            this.Controls.Add(this.comboBoxSize);
+            this.Controls.Add(this.btnSelectImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMainApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片转换Icon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.ComboBox comboBoxSize;
+        private System.Windows.Forms.Button buttonChangeIcon;
+        private System.Windows.Forms.TextBox textBoxImagePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialogImage;
+    }
+}
+
---
+++ FormMainApp.cs
@@ -0,0 +1,90 @@
+﻿using ImageToIconApp.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToIconApp
+{
+    public partial class FormMainApp : Form
+    {
+        public FormMainApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            this.openFileDialogImage.Filter = "(*.jpg)|*.jpg|(*.png)|*.png";
+            DialogResult result=  this.openFileDialogImage.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var fileName = this.openFileDialogImage.FileName;
+                if (!string.IsNullOrEmpty(fileName)) 
+                {
+                    this.textBoxImagePath.Text = fileName;
+                }
+            }
+        }
+
+        private void buttonChangeIcon_Click(object sender, EventArgs e)
+        {
+            var imagePath = this.textBoxImagePath.Text;
+           var fileName = Path.GetFileNameWithoutExtension(imagePath);
+            string outFilePath =$"{AppDomain.CurrentDomain.BaseDirectory}\\{fileName}.ico" ;
+            if (!string.IsNullOrEmpty(imagePath)) 
+            {
+                var imageSize =  this.comboBoxSize.Text;
+                var size = getConvertSize(imageSize);
+
+                IconHelper.ConvertPNGToIcon(imagePath, outFilePath,size,false);
+            }
+        }
+        /// <summary>
+        /// 16*16 24*24 32*32 48*48 64*64 72*72 92*92 108*108
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        private int getConvertSize(string size) 
+        {
+            int sizeImgage = 16;
+            switch (size)
+            {
+                case "16*16":
+                    sizeImgage=16;
+                    break;
+                case "24*24":
+                    sizeImgage = 24;
+                    break;
+                case "32*32":
+                    sizeImgage = 32;
+                    break;
+                case "48*48":
+                    sizeImgage = 48;
+                    break;
+                case "64*64":
+                    sizeImgage = 64;
+                    break;
+                case "72*72":
+                    sizeImgage=72;
+                    break;
+                case "92*92":
+                    sizeImgage = 92;
+                    break;
+                case "108*108":
+                    sizeImgage = 108;
+                    break;
+                default:
+                    sizeImgage = 32;
+                    break;
+            }
+            return sizeImgage;
+        }
+    }
+}
